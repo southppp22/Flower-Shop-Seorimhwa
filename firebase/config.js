4c65ca30598e063fc3c94af53d7c8f32fc025665
southppp22 <tnddl032@gmail.com>
2021-05-11 13:20:07 +0900

    [client]Add eslint

--- firebase/config.js
+++ firebase/config.js
@@ -5,13 +5,13 @@ import "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-	apiKey: "AIzaSyCagh_FO0YSfIqYX38SDMWnFB5Scrtc0Cw",
-	authDomain: "seorimhwa-project.firebaseapp.com",
-	projectId: "seorimhwa-project",
-	storageBucket: "seorimhwa-project.appspot.com",
-	messagingSenderId: "28523443097",
-	appId: "1:28523443097:web:c2a1f2ae919a7d86c89908",
-	measurementId: "G-6MFLD0FYS5",
+  apiKey: "AIzaSyCagh_FO0YSfIqYX38SDMWnFB5Scrtc0Cw",
+  authDomain: "seorimhwa-project.firebaseapp.com",
+  projectId: "seorimhwa-project",
+  storageBucket: "seorimhwa-project.appspot.com",
+  messagingSenderId: "28523443097",
+  appId: "1:28523443097:web:c2a1f2ae919a7d86c89908",
+  measurementId: "G-6MFLD0FYS5",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-	return <div className='App'>seorimhwa</div>;
+  return <div className="App">seorimhwa</div>;
 }
 
 export default App;
