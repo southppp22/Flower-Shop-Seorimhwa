1e0fbf48f3b9f7539cb0e58c06ec44bb3465a3c2
southppp22 <tnddl032@gmail.com>
2021-05-21 16:56:59 +0900

    [client]ADD OrderItem Molecule Component

---
+++ src/components/molecules/OrderItem/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Image } from "../../index";
+
+import * as S from "./style";
+
+export interface Props {
+  title: string;
+  imgSrc: string;
+  price: string;
+  quantity: number;
+  //옵션 변경 모달창
+  to: string;
+}
+
+function OrderItem({
+  title,
+  imgSrc,
+  price,
+  quantity,
+  to,
+}: Props): React.ReactElement {
+  return (
+    <S.Container>
+      <S.InfoConatiner>
+        <S.ImageWrapper>
+          <Image src={imgSrc} alt={title} />
+        </S.ImageWrapper>
+        <S.ItemDetailContainer>
+          <S.TitleWrapper>{title}</S.TitleWrapper>
+          <S.QuantityWrapper>수량: {quantity}</S.QuantityWrapper>
+        </S.ItemDetailContainer>
+      </S.InfoConatiner>
+      <S.OptionWrapper>옵션 변경</S.OptionWrapper>
+      <S.PriceWrapper>{price} 원</S.PriceWrapper>
+      <S.DeleteBtn>
+        <S.DeleteIcon>&nbsp;</S.DeleteIcon>
+      </S.DeleteBtn>
+    </S.Container>
+  );
+}
+
+export default OrderItem;
